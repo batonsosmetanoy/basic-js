034fb0835439ef5921a2c8befb8daa5054ba1a92
batonsosmetanoy <madraimovmsh@gmail.com>
2020-03-16 10:56:08 +0500

    feat: dream-team task solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,9 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  let correctMembers = members.filter(item => typeof item === "string");
+  let trimNsort = correctMembers.map(item => item.trim().toUpperCase()).sort();
+  let firstLetters = trimNsort.map(item => item.split('').splice(0, 1).join(''));
+
+  return firstLetters.join('');
 };
