dd115383f18d67521f17048b0d96ab19a1a12c09
batonsosmetanoy <madraimovmsh@gmail.com>
2020-03-17 17:14:49 +0500

    feat: transform-array task solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,12 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error();
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === '--double-next') {result.push(arr[i+1]);}
+      else if (arr[i] === '--double-prev') {result.push(arr[i-1]);}
+      else if (arr[i] === '--discard-prev') {result.pop();}
+      else if (arr[i] === '--discard-next') {i++}
+      else result.push(arr[i]);
+    }
+    return result.filter(item => item !== undefined);
 };
